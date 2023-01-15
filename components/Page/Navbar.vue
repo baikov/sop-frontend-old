3349dc6cbe5076b6c9207bbab7827858347fe161
Alexey Baikov <baikov52@gmail.com>
2023-01-16 00:53:07 +0300

    add modal

--- components/Page/Navbar.vue
+++ components/Page/Navbar.vue
@@ -1,11 +1,29 @@
 <script lang="ts" setup>
 import { AppConfigInput } from '@nuxt/schema'
+import { ref } from 'vue'
+import {
+  TransitionRoot,
+  TransitionChild,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/vue'
 
 export interface IMenuItem {
   type: 'link' | 'button'
   text: string
   href?: any
   route?: any
+}
+
+const isOpen = ref(false)
+
+// actions
+const closeModal = () => {
+  isOpen.value = false
+}
+const openModal = () => {
+  isOpen.value = true
 }
 
 const { t } = useLang()
@@ -20,30 +38,32 @@ const menus = computed((): IMenuItem[] => [
   { type: 'link', text: 'Трубы стальные', route: { name: 'test' } },
   { type: 'link', text: t('pages.post.nav'), route: { name: 'post' } },
   { type: 'link', text: t('pages.setting.nav'), route: { name: 'setting' } },
-  {
-    type: 'button',
-    text: 'Заказать звонок',
-    route: { name: 'dashboard' },
-  },
+  // {
+  //   type: 'button',
+  //   text: 'Заказать звонок',
+  //   route: { name: 'dashboard' },
+  // },
 ])
 </script>
 
 <template>
   <BuilderNavbar>
-    <!-- <template #banner>
+    <template #banner>
       <div
-        class="text-white text-xs text-center py-1 px-4 lg:px-8 bg-primary-500 capitalize"
+        class="text-white text-sm font-extrabold text-right py-1 px-4 lg:px-8 max-w-8xl w-full mx-auto"
       >
         <span class="mr-1">
-          {{ $t('banners.welcome', { app_name: app.name }) }}
+          <!-- {{ $t('banners.welcome', { app_name: app.name }) }} -->
+          Актуальный прайс на 2023 год:
+          <!-- :text="$t('others.learn_more')" -->
           <Anchor
             class="underline font-bold"
-            :text="$t('others.learn_more')"
-            href="https://github.com/viandwi24/nuxt3-awesome-starter"
+            text="soptorg-price-2023.xlsx"
+            href="#"
           />
         </span>
       </div>
-    </template> -->
+    </template>
     <template #menu>
       <div class="relative hidden lg:flex items-center ml-auto">
         <nav
@@ -66,6 +86,14 @@ const menus = computed((): IMenuItem[] => [
                 class="font-extrabold capitalize"
                 :to="item.route ? item.route : undefined"
                 :href="item.href ? item.href : undefined"
+              />
+            </li>
+            <li>
+              <Button
+                text="Open"
+                size="xs"
+                class="font-extrabold capitalize"
+                @click="openModal"
               />
             </li>
           </ul>
@@ -146,4 +174,62 @@ const menus = computed((): IMenuItem[] => [
       </ActionSheet>
     </template>
   </BuilderNavbar>
+  <TransitionRoot appear :show="isOpen" as="template">
+    <Dialog as="div" @close="closeModal" class="relative z-10">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black bg-opacity-25" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-y-auto">
+        <div
+          class="flex min-h-full items-center justify-center p-4 text-center"
+        >
+          <TransitionChild
+            as="template"
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-200 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+            >
+              <DialogTitle
+                as="h3"
+                class="text-lg font-medium leading-6 text-gray-900"
+              >
+                Payment successful
+              </DialogTitle>
+              <div class="mt-2">
+                <p class="text-sm text-gray-500">
+                  Your payment has been successfully submitted. We’ve sent you
+                  an email with all of the details of your order.
+                </p>
+              </div>
+
+              <div class="mt-4">
+                <button
+                  type="button"
+                  class="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                  @click="closeModal"
+                >
+                  Got it, thanks!
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
