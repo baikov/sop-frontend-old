df5a0feb80eeabc45256fa56b96becb5f7735d72
Alexey Baikov <baikov52@gmail.com>
2023-01-16 00:50:46 +0300

    fix navbar banner

--- components/Builder/Navbar.vue
+++ components/Builder/Navbar.vue
@@ -53,7 +53,7 @@ const toggleOptions = (show?: boolean) => {
     ref="navbar"
     class="backdrop-filter backdrop-blur-md top-0 z-40 w-full flex-none transition-colors duration-300 lg:z-50 border-b border-gray-900/10 dark:border-gray-50/[0.2] bg-white/[0.5] dark:bg-slate-900/[0.5]"
   >
-    <div id="navbar-banner" class="banner">
+    <div id="navbar-banner" class="banner bg-primary-500">
       <slot name="banner" />
     </div>
     <div class="max-w-8xl w-full mx-auto">
